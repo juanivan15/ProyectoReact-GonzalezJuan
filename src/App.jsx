dfe4ad0836daca1d57def7364e20b7c3d2157ad4
juanivan <juanivangonz15@gmail.com>
2023-11-30 15:43:02 -0300

    Creacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  let alumno = "Juan";
+  let curso = "React JS";
+  return (
+    <div>
+      <h1>Hola, mi nombre es {alumno}</h1>
+      <h2>Estoy en el curso de {curso}</h2>
+    </div>
+  )
+}
+
+export default App
